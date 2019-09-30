84f6e7134b837d5ad78d82182c054f411fb428aa
wynncreations <wynncreations@gmail.com>
2019-09-30 10:12:59 -0500

    more server code.

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require("path");
 const dotenv = require("dotenv");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const history = require("connect-history-api-fallback");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json({
@@ -29,6 +31,9 @@ mongoose.connect(
     }
 );
 
+if (process.env.ENV === "production") {
+    app.use(history());
+}
 
 const port = process.env.PORT || 8001;
 
