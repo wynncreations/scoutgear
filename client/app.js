d8451a54661f27e4ccef5d2ad0e89049967a8916
wynncreations <wynncreations@gmail.com>
2019-10-08 08:58:31 -0500

    moved port assignment to after dotenv config

--- client/app.js
+++ client/app.js
@@ -4,11 +4,11 @@ const dotenv = require("dotenv");
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
+
 var hostname = '127.0.0.1';
 
 dotenv.config();
-
+var port = process.env.PORT || 5000;
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
