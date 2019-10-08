3787816f0072afb6a6e922acc263b45de9771c12
wynncreations <wynncreations@gmail.com>
2019-10-08 08:32:03 -0500

    added simple app.js to handle static files.

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+const dotenv = require("dotenv");
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+var hostname = '127.0.0.1';
+
+dotenv.config();
+
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
